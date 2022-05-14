3ec0a782090c9aaad654f58d8425a631d1b88843
Staffan Olsson <staffan@repos.se>
2022-05-14 07:33:22 +0200

    The operated statefulset isn't RollingUpdate anymore

--- redpanda/unoperate.sh
+++ redpanda/unoperate.sh
@@ -8,6 +8,7 @@ echo "    type: OnDelete"
 echo "which is an indication that it can no longer run without the operator"
 
 echo "Please confirm that this is an ephemeral test cluster"
+echo "For example: k3d cluster create redpanda --agents 3"
 kubectl config current-context
 read -p "Ok? [y/n] " ok && [ "$ok" = "y" ] || exit 1
 
@@ -15,12 +16,13 @@ read -p "Ok? [y/n] " ok && [ "$ok" = "y" ] || exit 1
 stat redpanda/redpanda-operator
 
 kubectl create namespace cert-manager
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.4.1/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.8.0/cert-manager.yaml
 kubectl -n cert-manager rollout status deploy cert-manager
 kubectl -n cert-manager rollout status deploy cert-manager-webhook
 
 kubectl create namespace redpanda-system
 kubectl apply -f redpanda/redpanda-operator-crd/redpanda-operator-crd.yaml
+sleep 5
 kubectl -n redpanda-system apply -k apply -k redpanda/redpanda-operator/
 kubectl -n redpanda-system rollout status deploy unhelm-redpanda-operator
 
@@ -49,11 +51,12 @@ for example in \
 
   kubectl -n redpanda-example apply -f $exampledir/example.yaml
   sleep 5
-  kubectl -n redpanda-example rollout status statefulset redpanda
   kubectl -n redpanda-example get service redpanda -o yaml > $exampledir/service.yaml
   kubectl -n redpanda-example get statefulset redpanda -o yaml > $exampledir/statefulset.yaml
   kubectl -n redpanda-example get configmap redpanda-base -o yaml > $exampledir/configmap.yaml
-  for i in $(seq 0 2); do
+  replicas=$(kubectl -n redpanda-example get statefulset redpanda -o jsonpath='{.spec.replicas}')
+  for i in $(seq 0 $((replicas - 1))); do
+    kubectl -n redpanda-example wait --for=condition=Ready pod/redpanda-$i
     kubectl -n redpanda-example exec redpanda-$i -- cat /etc/redpanda/redpanda.yaml > $exampledir/redpanda-$i.yaml || echo "Failed to get config from pod $i"
   done
   kubectl -n redpanda-example delete -f $exampledir/example.yaml
